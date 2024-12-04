8f2c84b77704cab2260b6421021a55928b726254
MigueldeHaroce <migueldeharoce@gmail.com>
2024-12-04 11:59:53 +0100

    Exemple of functions

--- practica1/index.js
+++ practica1/index.js
@@ -1,9 +1,16 @@
-$('.button').click(function() {
+$('.button').click(function(event) {
+    event.preventDefault();
+
+    var name = $('input[name="txt"]').val(); 
+    var action = $(this).attr('name'); 
+
     $.ajax({
-      type: "POST",
-      url: "some.php",
-      data: { name: "John" }
-    }).done(function( msg ) {
-      alert( "Data Saved: " + msg );
+        type: "POST",
+        url: "index.php",
+        data: { name: name, action: action }
+    }).done(function(response) {
+    
+        $('body').append(response);
+        console.log(response);
     });
-  });
+});
--- practica1/index.php
+++ practica1/index.php
@@ -1,3 +1,35 @@
+<!-- 
+    Exemple of calling function on php.
+    Note: the echoes or prints goes throught jquery
+-->
+
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['name'])) {
+    function abc($name){
+        echo "<p>Hello $name</p>";
+    }
+
+    function xyz($name){
+        echo "<p>Hello $name, this is the final message!</p>";
+    }
+    function jaja($name){
+        echo "<p>jajajaj</p>";
+    }
+
+    $name = $_POST['name'];
+    $action = $_POST['action'];
+
+    if ($action === 'insert') {
+        abc($name);
+    } elseif ($action === 'select') {
+        xyz($name);
+    } elseif ($action === 'jaja') {
+        jaja($name);
+    }
+
+    exit;
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,25 +37,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-
 </head>
 <body>
-    <form action="functioncalling.php">
+    <form action="index.php" method="post">
         <input type="text" name="txt" />
         <input type="submit" class="button" name="insert" value="insert" />
         <input type="submit" class="button" name="select" value="select" />
+        <input type="submit" class="button" name="jaja" value="jaja" />
+
     </form>
 
-    <?php
-        function abc($name){
-            // Your code here
-        }
-    ?>
     <script src="index.js"></script>
-    <script>
-        document.get("myAnchor").addEventListener("click", function(event){
-            event.preventDefault()
-        });
-    </script>
 </body>
 </html>
